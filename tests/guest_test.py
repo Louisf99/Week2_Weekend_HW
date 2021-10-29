e51eb46e15323f99499fc28efe1000cd7aa9d5a9
Louis Fletcher <Louisco1999@gmail.com>
2021-10-29 17:54:16 +0100

    added tests for guest class to check if guests have name - all passed

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -5,7 +5,7 @@ from classes.rooms import Room
 class TestGuest(unittest.TestCase):
     
     def setUp(self):
-        self.guest1 = Guest("harry", 35, "White Noise")
+        self.guest1 = Guest("Harry", 35, "White Noise")
         self.guest2 = Guest("Liam", 50, "Ticket To Ride")
         self.guest3 = Guest("Susan", 75, "American Boy")
         
@@ -14,4 +14,13 @@ class TestGuest(unittest.TestCase):
         self.room3 = Room("VIP Room", 20)
 
 
-        
+    def test_guest_has_name(self):
+        self.assertEqual("Harry", self.guest1.name)
+
+    def test_guest_has_name2(self):
+        self.assertEqual("Liam", self.guest2.name)
+    
+    def test_guest_has_name3(self):
+        self.assertEqual("Susan", self.guest3.name)
+
+    
