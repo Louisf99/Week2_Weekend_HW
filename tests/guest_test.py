04eb31cd0da7e6f0508074c5429c07254db18c01
Louis Fletcher <Louisco1999@gmail.com>
2021-10-29 18:08:52 +0100

    added tests in room_test to check room has name and capacity - all passed

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -8,10 +8,6 @@ class TestGuest(unittest.TestCase):
         self.guest1 = Guest("Harry", 35, "White Noise")
         self.guest2 = Guest("Liam", 50, "Ticket To Ride")
         self.guest3 = Guest("Susan", 75, "American Boy")
-        
-        self.room1 = Room("LED Room",40 )
-        self.room2 = Room("Pop Room", 50)
-        self.room3 = Room("VIP Room", 20)
 
 
     def test_guest_has_name(self):
--- tests/room_test.py
+++ tests/room_test.py
@@ -6,15 +6,34 @@ from classes.songs import Song
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.guest1 = Guest("harry", 35, "White Noise")
-        self.guest2 = Guest("Liam", 50, "Ticket To Ride")
-        self.guest3 = Guest("Susan", 75, "American Boy")
-        
         self.room1 = Room("LED Room",40 )
         self.room2 = Room("Pop Room", 50)
         self.room3 = Room("VIP Room", 20)
 
+        self.guest1 = Guest("harry", 35, "White Noise")
+        self.guest2 = Guest("Liam", 50, "Ticket To Ride")
+        self.guest3 = Guest("Susan", 75, "American Boy")
+        
         self.song1 = Song("White Noise", "Disclosure, Aluna George")
         self.song2 = Song("White Iverson", "Post Malone")
         self.song3 = Song("Durag Activity", "Baby Keem, Travis Scott")
+        
+
+    def test_room_has_room_name(self):
+        self.assertEqual("LED Room", self.room1.room_name)
+
+    def test_room_has_room_name2(self):
+        self.assertEqual("Pop Room", self.room2.room_name)
+
+    def test_room_has_room_name3(self):
+        self.assertEqual("VIP Room", self.room3.room_name)
+
+    def test_room_has_room_capacity(self):
+        self.assertEqual(40, self.room1.room_capacity)
+
+    def test_room_has_room_capacity2(self):
+        self.assertEqual(50, self.room2.room_capacity)
+        
+    def test_room_has_room_capacity3(self):
+        self.assertEqual(20, self.room3.room_capacity)
         
